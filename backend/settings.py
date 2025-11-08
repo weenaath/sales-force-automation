a59fd1e5b739c791b998bf1c90d8542ba3742562
Sakindu Weenath <121110518+weenaath@users.noreply.github.com>
2025-11-09 01:45:46 +0530

    connect db

--- backend/settings.py
+++ backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sales_db',
+        'USER': 'postgres',
+        'PASSWORD': 'YourPasswordHere',  # replace this
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
