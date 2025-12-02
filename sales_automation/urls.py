930827c91d7b3cf62f55a74313293567fba23603
Sakindu Weenath <121110518+weenaath@users.noreply.github.com>
2025-12-02 20:42:57 +0530

    update urls.py

--- sales_automation/urls.py
+++ sales_automation/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(template_name='sales_automation/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('add-sale/', views.add_sale, name='add_sale'),
 ]
