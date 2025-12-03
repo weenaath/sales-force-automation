2303debbfa3ec7b49825d30221848dab44558dfd
Sakindu Weenath <121110518+weenaath@users.noreply.github.com>
2025-12-03 12:06:43 +0530

    Update ALLOWED_HOSTS in settings.py to allow all hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xko@ljvh)dnsb%a=@=b^c8q-@-aq2n35zh7*ezaak#g=((1h#x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
